be675eada1fdbb9e30d297d8960a37cdb646ae4a
Giovanni Silvestri <gsit80@gmail.com>
2014-09-25 15:05:27 +0200

    First commit for bitcoin-analytics project

---
+++ dev/src/src/main/java/com/gconsulting/service/impl/PasswordTokenManager.java
@@ -0,0 +1,19 @@
+package com.gconsulting.service.impl;
+
+import com.gconsulting.model.User;
+
+
+public interface PasswordTokenManager {
+
+    /**
+     * {@inheritDoc}
+     */
+    String generateRecoveryToken(User user);
+
+    /**
+     * {@inheritDoc}
+     */
+    boolean isRecoveryTokenValid(User user, String token);
+
+    void invalidateRecoveryToken(User user, String token);
+}
