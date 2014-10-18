cc48c3f7b2db1917ade481bb53d3b1eff273e297
Giovanni Silvestri <gsit80@gmail.com>
2014-10-18 18:02:59 +0200

    Added Model layer

--- dev/src/src/main/java/com/gconsulting/model/Role.java
+++ dev/src/src/main/java/com/gconsulting/model/Role.java
@@ -32,7 +32,8 @@ import java.io.Serializable;
         )
 })
 public class Role extends BaseObject implements Serializable, GrantedAuthority {
-    private static final long serialVersionUID = 3690197650654049848L;
+   
+	private static final long serialVersionUID = 3690197650654049848L;
     private Long id;
     private String name;
     private String description;
--- dev/src/src/main/java/com/gconsulting/webapp/view/ArbitrageView.java
+++ dev/src/src/main/java/com/gconsulting/webapp/view/ArbitrageView.java
@@ -2,6 +2,8 @@ package com.gconsulting.webapp.view;
 
 import java.io.Serializable;
 
+import javax.annotation.PostConstruct;
+
 import com.gconsulting.webapp.action.BasePage;
 
 public class ArbitrageView extends BasePage implements Serializable {
@@ -11,4 +13,9 @@ public class ArbitrageView extends BasePage implements Serializable {
 	 */
 	private static final long serialVersionUID = -3592159590716219395L;
 
+	@PostConstruct
+	public void init() {
+		//Load arbs
+		
+	}
 }
