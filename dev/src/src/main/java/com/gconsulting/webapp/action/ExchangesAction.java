5e6ae26736f4898ff9c277641918b39de596a8dd
Giovanni Silvestri <gsit80@gmail.com>
2014-10-29 16:19:18 +0100

    Market: list and add

--- dev/src/src/main/java/com/gconsulting/webapp/action/ExchangesAction.java
+++ dev/src/src/main/java/com/gconsulting/webapp/action/ExchangesAction.java
@@ -20,8 +20,11 @@ public class ExchangesAction extends BasePage implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 4820984471948349094L;
-	
+
+//	private boolean isUpdate;
 	private ExchangeManager exchangeManager;
+	private Exchange selectedExchange = new Exchange();
+	private String code;
 
 	@Autowired
 	public void setExchangeManager(
@@ -31,9 +34,65 @@ public class ExchangesAction extends BasePage implements Serializable {
 
 	public ExchangesAction() {
 		setSortColumn("code"); // sets the default sort column
+//		isUpdate = false;
 	}
 
 	public List<Exchange> getExchanges() {
 		return sort(exchangeManager.getAllExchange());
+	}
+
+	public Exchange getSelectedExchange() {
+		return selectedExchange;
+	}
+
+	public void setSelectedExchange(Exchange selectedExchange) {
+		this.selectedExchange = selectedExchange;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String delete() {
+
+//		isUpdate = false;		
+		exchangeManager.delete(selectedExchange);
+		addMessage("exchanges.deleted");
+		return "list";
+	}
+
+	public String edit() {
+
+		if (code == null) {
+			code = new String(getParameter("code"));
+		}
+		selectedExchange = exchangeManager.getExchangeByCode(code);
+//		isUpdate = true;
+//		getRequest().setAttribute("isUpdate", isUpdate);
+		return "edit";
+	}
+
+	public String save() {
+
+		String key = new String("");
+		boolean isUpdate = (exchangeManager.getExchangeByCode(selectedExchange.getCode()) == null) ? false : true; 
+//		isUpdate = (Boolean) getRequest().getAttribute("isUpdate");
+		if(!isUpdate) {
+			exchangeManager.create(selectedExchange);
+			key = "exchanges.added";
+		} else {
+			exchangeManager.update(selectedExchange);
+			key = "exchanges.updated";			
+		}
+		addMessage(key);
+//		if(!isUpdate) {
+//			return "edit";			
+//		} else {
+			return "list";			
+//		}
 	}
 }
